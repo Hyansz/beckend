eea8ac95d80b1a9db173800eec6648cd23844710
Raihan Regita Harjuno <raihanregita.ppqita@gmail.com>
2023-08-30 10:29:28 +0700

    Project

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+// contoh simple sederhana
+/**
+ *  req adalah singaktan dari request yang isinya dikirim oleh client.
+ * res adalah singaktan dari response yang isinya kita kirim ke client
+ * contoh seperti data, json, html, dan codeHTTP (default:200)
+ */
+
+app.get('/', (req, res) => {
+    
+})
+
+// Cara Running
+app.listen(3000, () => {
+    console.log('Hai Bro, Your Server Have Running in localhost:3000');
+})
+
+// Jalannya dengan menulis node app.js di terminal
